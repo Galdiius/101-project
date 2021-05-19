3299e040e5db3e3d1bca2b70730a26440ff8fdd0
gustopa <gustopaMI@gmail.com>
2021-05-19 15:34:36 +0700

    Add project

---
+++ ytmp3-downloader/js/script.js
@@ -0,0 +1,28 @@
+let api_key ="AIzaSyAj4A6rtB25PqPnCTWOrYUxplp0JSPW73s"
+
+
+let btnConvert = document.getElementById('convert')
+let inputLink = document.getElementById('inputLink')
+let info  = document.getElementById('info')
+let download = document.getElementById('download')
+let valid = document.getElementById('valid')
+let tidakValid = document.getElementById('tidakvalid')
+btnConvert.addEventListener('click',function(){
+    let id = YouTubeGetID(inputLink.value)
+    valid.style.display = 'block'
+    tidakValid.style.display = 'none'
+    valid.setAttribute('src',`https://www.yt-download.org/api/widget/mp3/${id}`)
+})
+
+function YouTubeGetID(url){
+    let ID = '';
+    url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+    if(url[2] !== undefined) {
+      ID = url[2].split(/[^0-9a-z_\-]/i);
+      ID = ID[0];
+    }
+    else {
+      ID = url;
+    }
+      return ID;
+  }
